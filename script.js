aa6f057f6d2691b21805cbdb6d861a56fa6b305e
student1111111111 <46721376+student1111111111@users.noreply.github.com>
2019-04-11 05:46:24 -0400

    Зауваження у тексті

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Дано масив імен. Сформувати новий масив, який складається з імен, які починаються на задану літеру.
 
 let names = ['pasha', 'ivan', 'artem', 'vova', 'igor', 'artur']
-let fLetter = names.filter((x, i, arr) => arr[i][0] == 'a')
+let fLetter = names.filter((x, i, arr) => arr[i][0] == 'a')  Краще було використати копію елемента x 
+                                                             let fLetter = names.filter(x => x[0] == 'a')
 document.write(fLetter)
 
 
@@ -14,7 +15,7 @@ carWeigth.every(x => x < 2000) ? document.write("All cars are less than 2000") :
 // Дано масив, який зберігає кількість відвідувачів протягом тижня.З’ясувати, чи були дні, коли кількість відвідувачів була меншою за 10.
 
 let numberOfVisitors = ["15","34","28","3","7","50","9","62","30"]
-numberOfVisitors.every(x => x < 10) ? document.write("in a few days was less than 10 visitours") 
+numberOfVisitors.every(x => x < 10) ? document.write("in a few days was less than 10 visitours")   Тут логічніше було використати some
 : document.write("ther was more than 10 visitours in this days")
 
 
